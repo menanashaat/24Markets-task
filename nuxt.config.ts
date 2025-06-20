bc9e19192506cd66048a1d4b2c231a8b8e3e8b1f
menanashaat <admin@Win-Raag-mna>
2025-06-20 12:00:48 +0300

    add i18n and scss

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,16 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-05-15',
-  devtools: { enabled: true }
-})
+  compatibilityDate: "2025-05-15",
+  devtools: { enabled: true },
+   modules: [
+    ['@nuxtjs/i18n', {
+      lazy: true,
+      langDir: 'locales/',
+      defaultLocale: 'en',
+      locales: [
+        { code: 'en', file: 'en.json', name: 'English' },
+        { code: 'ar', file: 'ar.json', name: 'Arabic', dir: 'rtl' }
+      ]
+    }]
+  ],
+  css: ["@/assets/scss/main.scss"], // if you're adding SCSS too
+});
---
+++ pages/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    <h1>Welcome to My App</h1>
+    <p>This is the homepage.</p>
+    <h1>{{ $t('welcome') }}</h1>
+  </div>
+</template>
