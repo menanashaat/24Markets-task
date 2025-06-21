35aa6629535b941efe45660cadc402b9dad7fb55
menanashaat <admin@Win-Raag-mna>
2025-06-21 19:05:54 +0300

    delete unused components

--- middleware/redirect-by-ip.global.ts
+++ middleware/redirect-by-ip.global.ts
@@ -18,25 +18,4 @@ export default defineNuxtRouteMiddleware(async (to) => {
 });
  
 
-// export default defineNuxtRouteMiddleware(async (to) => {
-//   if (process.client || to.path !== '/') return;
 
-//   const supportedLocales = ['en', 'ar'];
-//   const arabicCountries = ['EG', 'SA', 'AE', 'IQ', 'JO', 'KW', 'LB', 'LY', 'MA', 'OM', 'QA', 'SD', 'SY', 'TN', 'YE', 'BH', 'DZ', 'PS'];
-
-//   try {
-//     let country = 'US';
-//     if (process.env.NODE_ENV === 'production') {
-//       const data = await $fetch('https://ipinfo.io/json?token=9e0b308dd002b3');
-//       country = data.country;
-//     } else {
-//       country = 'EG'; // Simulate Arabic for local test
-//     }
-
-//     const lang = arabicCountries.includes(country) ? 'ar' : 'en';
-
-//     return navigateTo(`/${lang}`, { redirectCode: 302 });
-//   } catch (err) { 
-//     return navigateTo('/en');
-//   }
-// });
