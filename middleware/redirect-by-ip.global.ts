1c22f5a3d758a5f6b25d159f5f0dbcaec9bcc39f
menanashaat <admin@Win-Raag-mna>
2025-06-21 18:01:56 +0300

    IP info

--- middleware/redirect-by-ip.global.ts
+++ middleware/redirect-by-ip.global.ts
@@ -7,8 +7,8 @@ export default defineNuxtRouteMiddleware(async (to) => {
 
   try {
     const c = await $fetch('https://ipinfo.io/json?token=9e0b308dd002b3'); // use your token
-    console.log("c", c)
-    const isArabic = arabicCountries.includes(ipData.country);
+     console.log("IP info:", c);
+    const isArabic = arabicCountries.includes(c.country);
  
     return navigateTo(`/${isArabic ? 'ar' : 'en'}`, { redirectCode: 302 });
   } catch (error) {
@@ -18,6 +18,25 @@ export default defineNuxtRouteMiddleware(async (to) => {
 });
  
 
+// export default defineNuxtRouteMiddleware(async (to) => {
+//   if (process.client || to.path !== '/') return;
 
+//   const supportedLocales = ['en', 'ar'];
+//   const arabicCountries = ['EG', 'SA', 'AE', 'IQ', 'JO', 'KW', 'LB', 'LY', 'MA', 'OM', 'QA', 'SD', 'SY', 'TN', 'YE', 'BH', 'DZ', 'PS'];
 
+//   try {
+//     let country = 'US';
+//     if (process.env.NODE_ENV === 'production') {
+//       const data = await $fetch('https://ipinfo.io/json?token=9e0b308dd002b3');
+//       country = data.country;
+//     } else {
+//       country = 'EG'; // Simulate Arabic for local test
+//     }
 
+//     const lang = arabicCountries.includes(country) ? 'ar' : 'en';
+
+//     return navigateTo(`/${lang}`, { redirectCode: 302 });
+//   } catch (err) { 
+//     return navigateTo('/en');
+//   }
+// });
